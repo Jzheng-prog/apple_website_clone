4d9b884167a2434783a5decb69723f3a4fc018bd
Jzheng-prog <zhengjohn017@gmail.com>
2024-06-29 21:31:25 -0400

    feature video and imgs added

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,22 @@ import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
 import Model from './components/Model'
+import Features from './components/Features'
 import * as Sentry from '@sentry/react'
 
 const  App = ()=> {
-  return (<button onClick={() => methodDoesNotExist()}>Break the world</button>);
-  // return (
+  // return (<button onClick={() => methodDoesNotExist()}>Break the world</button>);
+  return (
 
-  //   <main className = 'bg-black'>
-  //     <NavBar/>
-  //     <Hero/>
-  //     <Highlights/>
-  //     <Model/>
-  //   </main>
+    <main className = 'bg-black'>
+      <NavBar/>
+      <Hero/>
+      <Highlights/>
+      <Model/>
+      <Features/>
+    </main>
   
-  // )
+  )
 }
 
 export default Sentry.withProfiler(App);
